6414ec263488f5b70632d700814355394d18a649
jgoodson2 <jonathongoodson@clayton.edu>
2018-07-07 11:06:00 -0400

    add Android project scaffolding

---
+++ app/src/test/java/com/example/jgoodson2/cpsc6138_assignment3_timer_and_threads/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.jgoodson2.cpsc6138_assignment3_timer_and_threads;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
